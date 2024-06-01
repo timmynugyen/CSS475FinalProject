dcc18bec856fc8d169f6632f6b0a33a6fbecf072
timmynugyen <130505847+timmynugyen@users.noreply.github.com>
2024-05-31 23:19:44 -0700

    Update views.py
    added new registration input views

--- reservation/views.py
+++ reservation/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
-from .models import Reservation
+from .models import Reservation, Customer, TimeSlot, PoolOption, ServiceType, Service, RoomOption
 from .forms import Frontpage
 
 #Timmy: shows frontpage and gathers form information
@@ -8,10 +8,63 @@ def frontpage(request):
     if request.method == 'POST':
         form = Frontpage(request.POST)
         if form.is_valid():
-            form.save()
+            customer = form.save()
+
+            service_types = form.cleaned_data.get('service_type')
+            start_time = form.cleaned_data.get('start_time')
+            end_time = form.cleaned_data.get('end_time')
+            room_name = form.cleaned_data.get('room_name')
+            room_attendees = form.cleaned_data.get('room_attendees')
+            room_special_orders = form.cleaned_data.get('room_special_orders')
+            pool_name = form.cleaned_data.get('pool_name')
+            pool_attendees = form.cleaned_data.get('pool_attendees')
+            pool_special_orders = form.cleaned_data.get('pool_special_orders')
+
+            #Creates timeslot object
+            timeslot = TimeSlot.objects.create(start_time=start_time, end_time=end_time)
+            
+            #Creates pool_option object
+            pool_option = None
+            #sets to none, otherwise sets to selected pool
+            if 'pool' in service_types:
+                #creates pool_object based on selected pool
+                pool_option = PoolOption.objects.create(
+                    pool_name = pool_name,
+                    attendees = pool_attendees or 0,
+                    timeslot = timeslot,
+                    special_orders = pool_special_orders
+                )
+            
+            #Creates room_option object
+            room_option = None
+            #sets to none ,otherwise sets to selected room
+            if 'room' in service_types:
+                #creates room_option object base on selected room
+                room_option = RoomOption.objects.create(
+                    room_name = room_name,
+                    attendees = room_attendees or 0,
+                    timeslot = timeslot,
+                    special_orders = room_special_orders
+                )
+            
+            #creates service_type object based on pool/room options
+            service_type = ServiceType.objects.create(
+                pool_option = pool_option,
+                room_option = room_option
+            )
+            
+            service = Service.objects.create()
+            service.service_type.add(service_type)
+
+            #TODO
+            # Create reservation object from gather info above
+
             return redirect('submitted')
     else:
+        #TODO
+        #Display error message/error page saying reservation info is incorrect/already exists
         form = Frontpage()
+        
     return render(request, "frontpage.html", {'form': form})
 
 #Timmy: redirects to submitted page
