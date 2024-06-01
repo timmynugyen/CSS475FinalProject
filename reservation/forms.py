3c740a4e5e6d92bed5383ffda2b09e5e62ae6dda
timmynugyen <130505847+timmynugyen@users.noreply.github.com>
2024-05-31 23:19:10 -0700

    Update forms.py
    added other registration inputs

--- reservation/forms.py
+++ reservation/forms.py
@@ -1,7 +1,63 @@
 from django import forms
-from .models import Customer
+from .models import Customer, TimeSlot, RoomOption, PoolOption, ServiceType
 
 class Frontpage(forms.ModelForm):
+    ServiceChoices = [
+        ('pool', 'Pool'),
+        ('room', 'Room')
+    ]
+
+    service_type = forms.MultipleChoiceField(
+        required = True,
+        widget = forms.CheckboxSelectMultiple,
+        choices = ServiceChoices,
+        label = "Select Reservation Type"
+    )
+
+    start_time = forms.DateTimeField(
+        required=True,
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        label="Start Time"
+    )
+    
+    end_time = forms.DateTimeField(
+        required=True,
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        label="End Time"
+    )
+    
+    room_name = forms.ChoiceField(
+        choices=RoomOption.RoomTypes.choices,
+        label="Room Option"
+    )
+    
+    room_attendees = forms.IntegerField(
+        required=False,
+        label="Room Attendees"
+    )
+    
+    room_special_orders = forms.CharField(
+        required=False,
+        widget=forms.Textarea,
+        label="Room Special Orders"
+    )
+    
+    pool_name = forms.ChoiceField(
+        choices=PoolOption.PoolTypes.choices,
+        label="Pool Option"
+    )
+    
+    pool_attendees = forms.IntegerField(
+        required=False,
+        label="Pool Attendees"
+    )
+    
+    pool_special_orders = forms.CharField(
+        required=False,
+        widget=forms.Textarea,
+        label="Pool Special Orders"
+    )
+
     class Meta:
         model = Customer
         fields = ['first_name', 'last_name', 'email', 'phone_number']
